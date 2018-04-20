fba26a29d8e663e9a418f9187b3da7d2d25e3a4f
Timor <timor.ermanov@gmail.com>
2018-04-20 17:15:26 +0300

    add changes in disable decorator

---
+++ decorators/disable_decorator.py
@@ -0,0 +1,15 @@
+from functools import wraps
+from time import time
+
+
+def cancelled(func):
+    """Cancels the execution of a function"""
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        print(func.__name__, 'is cancelled!')
+    return wrapped
+
+
+# @cancelled
+# def echo_value(arg):
+#     print(arg)

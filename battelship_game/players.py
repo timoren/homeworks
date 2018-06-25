6a38e45912c215cc37ee9ddcd672e7d55f52afc0
Timor Ermanov <timore@mellanox.com>
2018-06-25 08:54:14 +0300

    add more core for seabattle

--- battelship_game/players.py
+++ battelship_game/players.py
@@ -19,3 +19,23 @@ class Human(Player):
         self.name = name
         self.graphic = graphic
 
+    def setup_ship(self):
+        """
+        setting ships on the board
+        :return: None
+        """
+
+        print('\n\n\t\tplayer {} setting his ships on the board'.format(self.name))
+        self.graphic.draw_board(self.board)
+        for count_deck, count_ship in Board.SHIPS_COUNT.items():
+            for i in range(count_ship):
+                while True:
+                    if count_deck == 1:
+                        position = input('\n enter position for {} deck ship example (row,col) (1a or 4f...):\n'.format(count_deck))
+                        if self.validate_position_install(position):
+                            direction = ''
+                            break
+                        else:
+                            position_direction = input('\n  enter position for {} deck ship example vector (n, s, w, e) (1a, 4f ...)'.format(count_deck))
+                            if self.validation_position_direction((position_direction, count_deck))
+
